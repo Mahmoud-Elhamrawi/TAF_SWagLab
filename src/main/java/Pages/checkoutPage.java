939f21c1d24c710d39fc604ea8ceebad42521994
mahmoudelhamrawi <mahmoudelhamrawi5@gmail.com>
2023-11-14 16:54:57 +0200

    add checkout func

--- src/main/java/Pages/checkoutPage.java
+++ src/main/java/Pages/checkoutPage.java
@@ -1,4 +1,75 @@
 package Pages;
 
-public class checkoutPage {
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class checkoutPage extends pageBase{
+    public checkoutPage(WebDriver driver) {
+        super(driver);
+    }
+
+    private final By addToCart_bike_light = By.id("add-to-cart-sauce-labs-bike-light") ;
+    private final By addToCart_labs_backpack = By.id("add-to-cart-sauce-labs-backpack");
+
+    private final By assertCountCart = By.className("shopping_cart_badge");
+
+    private final By goToCartPage = By.cssSelector("a[class=\"shopping_cart_link\"]");
+    private final By checkoutBtn = By.id("checkout");
+    private final By FnameInp = By.id("first-name");
+    private final By LnameInp = By.id("last-name");
+    private  final By postCodeInp = By.id("postal-code");
+    private final By countiunBtn = By.id("continue");
+    private final By assrtTitle = By.cssSelector("span[class=\"title\"]");
+    private final By finishBtn = By.id("finish");
+    private final By assertCompelet =By.className("complete-header");
+    private final By backBtn = By.id("back-to-products");
+    public void addToCart()
+    {
+        clickEle(driver.findElement(addToCart_bike_light));
+        clickEle(driver.findElement(addToCart_labs_backpack));
+    }
+
+    public WebElement countOfCart()
+    {
+        return driver.findElement(assertCountCart);
+    }
+
+    public void cartPage()
+    {
+        clickEle(driver.findElement(goToCartPage));
+    }
+
+    public void clickingOnCheckout()
+    {
+        clickEle(driver.findElement(checkoutBtn));
+    }
+
+    public void fillPersonalData(String FN , String LN ,String PC)
+    {
+        enterTxt(driver.findElement(FnameInp),FN );
+        enterTxt(driver.findElement(LnameInp),LN );
+        enterTxt(driver.findElement(postCodeInp),PC );
+        clickEle(driver.findElement(countiunBtn));
+    }
+
+      public  WebElement assertTit()
+{
+    return driver.findElement(assrtTitle);
+}
+
+       public void finishCheck()
+   {
+       clickEle(driver.findElement(finishBtn));
+   }
+
+    public  WebElement assertCom()
+    {
+        return driver.findElement(assertCompelet);
+    }
+    public void backToHome()
+    {
+        clickEle(driver.findElement(backBtn));
+    }
+
 }
--- src/test/java/TestCases/checkoutTest.java
+++ src/test/java/TestCases/checkoutTest.java
@@ -1,4 +1,100 @@
 package TestCases;
 
-public class checkoutTest {
+import Pages.autheniticationPage;
+import Pages.checkoutPage;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+
+import java.time.Duration;
+
+public class checkoutTest  extends testBase{
+
+    autheniticationPage autheniticationPage ;
+    SoftAssert softAssert = new SoftAssert();
+
+
+checkoutPage checkoutPage ;
+
+    @Test
+    public void checkoutFunc()
+    {
+        //login
+        softAssert.assertTrue(driver.getCurrentUrl().contains("https://www.saucedemo.com/"));
+
+        autheniticationPage = new autheniticationPage(driver);
+        autheniticationPage.loginFunc("standard_user","secret_sauce");
+        softAssert.assertTrue(driver.getCurrentUrl().contains("https://www.saucedemo.com/inventory.html"));
+        System.out.println(driver.getCurrentUrl());
+
+        String title ="Swag Labs" ;
+        String expectTitile = autheniticationPage.assertTit().getText();
+
+
+        //add to cart//
+
+
+        checkoutPage = new checkoutPage(driver);
+        checkoutPage.addToCart();
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.visibilityOf(checkoutPage.countOfCart()));
+
+         String actualCount = "2" ;
+         softAssert.assertEquals(checkoutPage.countOfCart().getText() , actualCount);
+
+         //go to cart //
+         checkoutPage.cartPage();
+         softAssert.assertTrue(driver.getCurrentUrl().contains("https://www.saucedemo.com/cart.html"));
+
+         //go to checkout//
+        checkoutPage.clickingOnCheckout();
+         softAssert.assertTrue(driver.getCurrentUrl().contains("https://www.saucedemo.com/checkout-step-one.html"));
+
+         checkoutPage.fillPersonalData("mahmoud","rashad","252525");
+         softAssert.assertTrue(driver.getCurrentUrl().contains("https://www.saucedemo.com/checkout-step-two.html"));
+
+         String actualTxt ="Checkout: Overview" ;
+
+         softAssert.assertEquals(actualTxt , checkoutPage.assertTit().getText());
+         checkoutPage.finishCheck();
+
+         softAssert.assertTrue(driver.getCurrentUrl().contains("https://www.saucedemo.com/checkout-complete.html"));
+
+
+         softAssert.assertTrue(checkoutPage.assertCom().getText().contains("Thank you for your o"));
+
+         checkoutPage.backToHome();
+         softAssert.assertTrue(driver.getCurrentUrl().contains("https://www.saucedemo.com/inventory.html"));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        softAssert.assertAll();
+
+
+
+
+
+    }
+
+
+
+
+
 }
